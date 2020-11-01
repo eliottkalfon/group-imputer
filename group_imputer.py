b3da93fbf2bd5195980a28a705059916463a0afe
Eliott Kalfon <eliott.kalfon@gmail.com>
2020-11-01 15:07:15 +0000

    Custom imputation function

--- group_imputer.py
+++ group_imputer.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 29 07:37:05 2020
 
 @author: eliott.kalfon
 
@@ -34,35 +33,62 @@ class GroupImputer():
     (or custom function, to be applied to the array)
     '''
     def __init__(self, strategy = 'mean'):
-        self.col_means = {}
-        self.group_means = {}
+        self.col_imputation_values = {}
+        self.group_imputation_values = {}
         self.strategy = strategy
         self.groupby_column = None
         self.group_values = None
         self.imputed_columns = None
+        self._impcol_isstring = None
+        # Getting the imputation function based on user input
+        if isinstance(self.strategy, str):
+            if self.strategy.lower() == 'mean':
+                self.imputation_function = lambda df: df.mean()
+            elif self.strategy.lower() == 'median':
+                self.imputation_function = lambda df: df.median()
+            elif self.strategy.lower() == 'min':
+                self.imputation_function = lambda df: df.min()
+            elif self.strategy.lower() == 'max':
+                self.imputation_function = lambda df: df.max()
+            else:
+                raise ValueError('Please enter a valid imputation strategy ("mean", "median", "min", "max")')   
+        # Checks if imputation strategy is a function
+        # The custom function must take a dataframe as argument and return either a series (for multiple columns) or a scalar (for single columns)
+        elif callable(self.strategy):
+            self.imputation_function = self.strategy
+        else:
+            raise ValueError('Please enter either a string ("mean", "median", "min", "max") or a custom function as an imputation strategy')
         
     def fit(self, data, groupby_column, imputed_columns):
         # Updating the groupy and imputed column attributes
         self.groupby_column = groupby_column
         self.imputed_columns = imputed_columns
+        #Checking if the imputed columns input is a list
+        self._impcol_isstring = isinstance(self.imputed_columns, str)
         # Getting the unique group values
         self.group_values = data[groupby_column].unique()
         # Creating a list of selected columns, composed of imputed and groupby columns
-        selected_columns = self.imputed_columns + [self.groupby_column]
+        if self._impcol_isstring:
+            selected_columns = [self.imputed_columns] + [self.groupby_column]
+        else:
+            selected_columns = self.imputed_columns + [self.groupby_column]
         # Getting a dictionary of means by column/group
-        self.group_means = df[selected_columns].groupby(by=self.groupby_column).mean().to_dict()
+        self.group_imputation_values = self.imputation_function(df[selected_columns].groupby(by=self.groupby_column)).to_dict()
         # Getting a dictionary of means by column (used for imputation when a group is filled with NAs)
-        self.col_means = df[self.imputed_columns].mean().to_dict()
+        if self._impcol_isstring:
+            self.col_imputation_values = {self.imputed_columns:self.imputation_function(df[self.imputed_columns])}
+        else:    
+            self.col_imputation_values = self.imputation_function(df[self.imputed_columns]).to_dict()
         
     def impute_column(self, column_name, data):
         # Iteration over groups
         for group in self.group_values:
             # Imputation value selection
             # If the group mean is null, then use the column mean
-            if self.group_means[column_name][group] == self.group_means[column_name][group]:
-                imputation_value = self.group_means[column_name][group]
+            if self.group_imputation_values[column_name][group] == self.group_imputation_values[column_name][group]:
+                imputation_value = self.group_imputation_values[column_name][group]
             else:
-                imputation_value = self.col_means[column_name]
+                imputation_value = self.col_imputation_values[column_name]
             # Filtering the data with pandas loc operator
             data.loc[
                 # Filtering to a single group
@@ -76,7 +102,7 @@ class GroupImputer():
         return data
     
     def transform(self, data):
-        if isinstance(self.imputed_columns, str):
+        if self._impcol_isstring:
             data = self.impute_column(column_name = self.imputed_columns, data = data)
         else:
             # Iteration over columns to impute
@@ -88,8 +114,6 @@ class GroupImputer():
         self.fit(data,groupby_column,imputed_columns)
         self.transform(data)
 
-gi = GroupImputer()
-# gi.fit(df, 'column1', ['column2', 'column3'])
-# gi.transform(df)
+gi = GroupImputer(strategy = 'median')
 gi.fit_transform(df,'column1', ['column2', 'column3'])
 print(df)
